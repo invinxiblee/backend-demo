f660a7018706fdb752f19dae2f5ba7d060c4ea32
Mo <mulla6777@gmail.com>
2026-04-26 14:39:59 +0530

    added env variables

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const express = require("express");
 const app = express();
 
+// PORT from environment
 const PORT = process.env.PORT || 3000;
 
+// SECRET from environment
+const SECRET = process.env.JWT_SECRET || "defaultsecret";
+
 app.get("/", (req, res) => {
-  res.send("Backend is running");
+  res.send("Backend is running with env variables");
+});
+
+app.get("/secret", (req, res) => {
+  res.send("Secret is: " + SECRET);
 });
 
 app.listen(PORT, () => {
